2f4d7ab16b7bf01e12df458fac25a7d2c9f50022
aryapraset <aryaprasetiyo12@gmail.com>
2023-05-11 11:00:13 +0800

    "local storage implemented"

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
+/* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable no-underscore-dangle */
 // mengimpor dotenv dan menjalankan konfigurasinya
 require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
 const Jwt = require('@hapi/jwt');
+const Inert = require('@hapi/inert');
 const path = require('path');
 
 // notes
@@ -58,6 +60,9 @@ const init = async () => {
   await server.register([
     {
       plugin: Jwt,
+    },
+    {
+      plugin: Inert,
     },
   ]);
 
